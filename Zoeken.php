4bd61f5fa1110de28fdcf630e6d65a76e5ececd8
Christian Bosman <christianps23@hotmail.com>
2019-11-14 13:05:18 +0100

    MYSQLI Statement prepared

--- Zoeken.php
+++ Zoeken.php
@@ -1,28 +1,37 @@
 <?php
 include "databaseConnection.php";
 $search = "%" . $_GET['zoeken'] . "%";
-
+$search2 = $_GET['zoeken'];
 
 $searchQuery = "SELECT * FROM stockitems WHERE StockItemName LIKE ? OR StockItemID = ? OR SearchDetails LIKE ?";
 
 $searchSQL = mysqli_prepare($connection, $searchQuery);
-mysqli_stmt_bind_param($searchSQL, 'sss', $search, $search, $search);
+mysqli_stmt_bind_param($searchSQL, 'sss', $search,$search2, $search);
 mysqli_stmt_execute($searchSQL);
 $result = mysqli_stmt_get_result($searchSQL);
 ?>
 
 <html>
 <body>
-    <?php
-    if(mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-            $StockItemName = $row["StockItemName"];
-            $StockItemID = $row["StockItemID"];
-            print($StockItemName."<BR>");
+<?php
+
+
+if(mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        $StockItemName = $row["StockItemName"];
+        $StockItemID = $row["StockItemID"];
+        if ($StockItemID == $search2) {
+            print($StockItemName);
+            break;
+        } else {
+            print($StockItemName . "<br>");
         }
-    }else{
-        print("No results");
     }
-    ?>
+}else {
+    print("No Results");
+}
+
+
+?>
 </body>
 </html>
