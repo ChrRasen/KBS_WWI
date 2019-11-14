19e28bc94c3f383f7aace3f8963df720b960b081
Jelle-Jetze <jellejetze.hoekstra@student.hu.nl>
2019-11-14 14:55:45 +0100

    limit en offset
    zorgt er voor dat je met buttons kan kijken naar de volgende 25/50/100 producten

--- Categorie.php
+++ Categorie.php
@@ -2,6 +2,11 @@
 session_start();
 include "databaseConnection.php";
 //
+if(isset($_SESSION["offset"])){
+    $offset = $_SESSION["offset"];
+}else{
+    $offset = 0;
+}
 
 if(isset($_GET["CAT"])){
     $categorieNaam = $_GET["CAT"];
@@ -14,6 +19,17 @@ if(!isset($_GET["aantal"])){
 }else{
     $limit = $_GET["aantal"];
 }
+if(isset($_GET["pagina"])){
+    if($_GET["pagina"] == "terug"){
+        $offset = $offset -1;
+    }else{
+        $offset = $offset +1;
+    }
+}
+$offsetSQL = $offset * $limit;
+
+$offsetSQL = $limit * $offset;
+$_SESSION["offset"] = $offset;
 $_SESSION["CAT"] = $categorieNaam;
 print($categorieNaam);
 //$result = mysqli_query($connection, "SELECT * FROM stockitems");
@@ -23,16 +39,18 @@ FROM stockitems S JOIN stockitemstockgroups SI
 ON S.stockitemid = SI.StockItemID JOIN stockgroups SG
 ON SI.StockGroupID = SG.StockGroupID
 WHERE StockGroupName =?
-LIMIT ?";
+LIMIT ? OFFSET ?";
 
 $statement = mysqli_prepare($connection, $categorie);
-mysqli_stmt_bind_param($statement, 'si', $categorieNaam, $limit);
+mysqli_stmt_bind_param($statement, 'sii', $categorieNaam, $limit, $offsetSQL);
 mysqli_stmt_execute($statement);
 $result = mysqli_stmt_get_result($statement);
+
 
 ?>
 <html>
 <body>
+<br>
 <?php
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 {
@@ -43,13 +61,14 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 }
 ?>
     <form action="Categorie.php" method="GET">
+        <?php if($offset != 0){echo'
+        <input type="submit" value="terug" name="pagina">
+        ';} ?>
         <input type="submit" value=25 name="aantal">
         <input type="submit" value=50 name="aantal">
         <input type="submit" value=100 name="aantal">
-
+        <input type="submit" value="volgende" name="pagina">
     </form>
 </body>
 </html>
-
-
 
