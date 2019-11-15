260df83e1320e86f6279cca07481b8298e463793
Jelle-Jetze <jellejetze.hoekstra@student.hu.nl>
2019-11-15 13:12:46 +0100

    categorieën
     bij eerste pagina kan je niet meer terug.
    en als je naar index gaat worden bebaalde session waardes gereset

--- Categorie.php
+++ Categorie.php
@@ -1,24 +1,30 @@
 <?php
 session_start();
 include "databaseConnection.php";
+
 //zorgt er voor dat de juiste waardes er zijn
 if(isset($_SESSION["offset"])){
     $offset = $_SESSION["offset"];
 }else{
     $offset = 0;
 }
+
 //zorgt er voor dat het de juiste categorie laat zien
 if(isset($_GET["CAT"])){
     $categorieNaam = $_GET["CAT"];
 }else{
     $categorieNaam = $_SESSION["CAT"];
 }
+
 //zorgt er voor dat het de juiste aantal prducten laat zien (25, 50 of 100)
-if(!isset($_GET["aantal"])){
+if(!isset($_GET["aantal"]) && !isset($_SESSION["Saantal"])){
     $limit = 25;
-}else{
+}else if(isset($_GET["aantal"])){
     $limit = $_GET["aantal"];
+}else if(isset($_SESSION["Saantal"])){
+    $limit = $_SESSION["Saantal"];
 }
+
 //zorgt er voor dat het de goede producten laat zien (0 tot 25 of 26 tot 50 etc.)
 if(isset($_GET["pagina"])){
     if($_GET["pagina"] == "terug"){
@@ -27,12 +33,19 @@ if(isset($_GET["pagina"])){
         $offset = $offset +1;
     }
 }
+if($offset < 0){
+    $offset = 0;
+}
 $offsetSQL = $offset * $limit;
 
 $offsetSQL = $limit * $offset;
 $_SESSION["offset"] = $offset;
 $_SESSION["CAT"] = $categorieNaam;
-print($categorieNaam);
+$_SESSION["Saantal"] = $limit;
+print($categorieNaam. "<br>");
+print($limit. "<br>");
+print($offset. "<br>");
+print($offsetSQL. "<br>");
 //$result = mysqli_query($connection, "SELECT * FROM stockitems");
 
 //de sql query om de producten te laten zien
@@ -42,6 +55,9 @@ ON S.stockitemid = SI.StockItemID JOIN stockgroups SG
 ON SI.StockGroupID = SG.StockGroupID
 WHERE StockGroupName =?
 LIMIT ? OFFSET ?";
+
+//sql query voor een count zodat je kan kijken of die niet veder kan gaan en maximum aantal pagina's kan berekenen
+
 
 $statement = mysqli_prepare($connection, $categorie);
 mysqli_stmt_bind_param($statement, 'sii', $categorieNaam, $limit, $offsetSQL);
@@ -67,10 +83,15 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
         if($offset != 0){echo'
         <input type="submit" value="terug" name="pagina">
         ';} ?>
+        <?php
+        //while loop die zorgt dat je snell terug kan gaan of veder kan gaan
+
+        ?>
+        <input type="submit" value="volgende" name="pagina">
+        <br>
         <input type="submit" value=25 name="aantal">
         <input type="submit" value=50 name="aantal">
         <input type="submit" value=100 name="aantal">
-        <input type="submit" value="volgende" name="pagina">
     </form>
 </body>
 </html>
