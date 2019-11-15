5f9aa1f237a6dc2e74d6b8e48594295f83b570fe
xXByakuXx <57754963+xXByakuXx@users.noreply.github.com>
2019-11-15 13:16:42 +0100

    Revert "Update Zoeken.php"
    This reverts commit 705fc673b35a09a20c4ebadf23b4f5b418b7f9db.

--- Zoeken.php
+++ Zoeken.php
@@ -2,6 +2,7 @@
 include "databaseConnection.php";
 $search = "%" . $_GET['zoeken'] . "%";
 $search2 = $_GET['zoeken'];
+
 $searchQuery = "SELECT * FROM stockitems WHERE StockItemName LIKE ? OR StockItemID = ? OR SearchDetails LIKE ?";
 
 $searchSQL = mysqli_prepare($connection, $searchQuery);
@@ -11,28 +12,22 @@ $result = mysqli_stmt_get_result($searchSQL);
 ?>
 
 <html>
-<head></head>
-<style>
-    img {
-        width:150px;
-    }
-</style>
 <body>
 <?php
-if( $_GET['zoeken'] == ""){
-    print("Vul iets in!");
-}elseif(mysqli_num_rows($result) > 0) {
+
+
+if(mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         $StockItemName = $row["StockItemName"];
         $StockItemID = $row["StockItemID"];
         $StockItemPrice = $row['UnitPrice'];
-        $StockPhoto = '<img src="data:image/jpg;base64,' . $row['Photo'] . '">';
-        Print('<a href="http://localhost/Project%20KBS/Product.php?ProductID=' . $StockItemID . '">' . $StockPhoto . '</a><br>');
-        print($StockItemName . "<br> " . $StockItemPrice . "<br><br>");
+        print($StockItemName ."<br> ". $StockItemPrice. "<br><br>");
     }
 }else {
-    print("Geen resultaten");
+    print("No Results");
 }
+
+
 ?>
 </body>
 </html>
