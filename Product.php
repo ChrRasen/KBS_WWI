5d9ca6ad3f2c43b65c082bb678fef5d2239b2da7
JochemBaan <39567302+JochemBaan@users.noreply.github.com>
2019-11-19 08:48:11 +0100

    Update Product.php

--- Product.php
+++ Product.php
@@ -1,1 +1,36 @@
+<html>
+
 <?php
+session_start();
+$host = "localhost";
+$databasename = "wideworldimporters";
+$user = "root";
+$pass = "";
+$port = 3306;
+$connection = mysqli_connect($host,$user,$pass,$databasename,$port);
+
+$StockID = $_GET["ProductID"];
+
+$stockItemDetails = mysqli_query($connection, "SELECT Video, UnitPrice, StockItemName FROM stockitems WHERE StockItemID = $StockID");
+$resultStockItemDetails = mysqli_fetch_array($stockItemDetails);
+
+$stock = mysqli_query($connection, "SELECT QuantityOnHand FROM stockitemholdings WHERE StockItemID = $StockID");
+$resultStock = mysqli_fetch_array($stock);
+
+$photo = mysqli_query($connection, "SELECT Photo FROM stockitems WHERE StockItemID = $StockID");
+//if(isset($photo)) {
+    $resultPhoto = mysqli_fetch_array($photo);
+//}else{
+    //$resultPhoto =  "<img src='no_image.png'>";
+//}
+?>
+<body>
+    <?php
+    print($resultStockItemDetails["StockItemName"] . '<br>');
+    echo '<a target="_blank" href="'. $resultStockItemDetails["Video"] .'">Video</a><br>';
+    print("price per unit: " . $resultStockItemDetails["UnitPrice"] . '<br>');
+    print("Quantity on hand:  " . $resultStock["QuantityOnHand"] . '<br>');
+    echo '<img src="'.$resultPhoto['Photo'].'" alt="foto"/>' . '<br>';
+    ?>
+</body>
+</html>
