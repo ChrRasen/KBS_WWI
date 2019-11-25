94f8f697485ba6249574ae69389533c9fc68ab5f
Bastiaanf98 <57759083+Bastiaanf98@users.noreply.github.com>
2019-11-25 15:05:40 +0100

    frontend inlog pagina

--- Inlogpagina.php
+++ Inlogpagina.php
@@ -1,5 +1,15 @@
 <html>
-<body>
-
+<head>
+    <link rel="stylesheet" type="text/css" href="stylesheet KBS.css">
+</head>
+<body >
+<div class="center-text">
+<h1>Inloggen</h1>
+</div>
+<form class="center-block">
+Gebruikersnaam: <input  type="text" name="gebruikersnaam"><br>
+Wachtwoord: <input type="password" name="wachtwoord"><br>
+<input  type="submit" name="submit" value="Inloggen">
+</form>
 </body>
 </html>
