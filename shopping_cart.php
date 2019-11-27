446d238f1b7ad40c0a6fbbac140d4d3ba9050c24
Jelle-Jetze <jellejetze.hoekstra@student.hu.nl>
2019-11-27 12:49:35 +0100

    kleine shopping cart fix
    zitten nog bugs in

--- shopping_cart.php
+++ shopping_cart.php
@@ -3,25 +3,32 @@
 <body>
 <?php
 session_start();
-$aantal = array();
 $producten = array("Jelle", "Christian", "Andy");
-if(isset ($_POST["erbij"])) {
-    $producten = $_POST["erbij"];
+if(isset ($_GET["erbij"])) {
+    $producten = $_GET["erbij"];
 }
+
 $i = 0;
+if(!isset($_GET[$i])) {
+    $aantal = array(1,1,1);
+}else{
+    $n = 0;
+    $aantal= array();
+    foreach ($producten as $index){
+        $aantal[$n] = $_GET[$n];
+    }
 
-
+}
 
 foreach($producten as $index => $waarde){
 
-    echo '<form action="shopping_cart.php" method="post">
+    echo '<form action="shopping_cart.php" method="get">
     ' .$waarde.'
-<input type="number" min="1" name="hoeveelheid" value="1" id="'.$i.'" class="calculator-input" onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>
-    <button type="submit" value="submit">kies</button>
-    
-    </form>';
-    if(isset($_POST["hoeveelheid"])) {
-        $hoeveelheid = $_POST["hoeveelheid"];
+<input type="number" min="1" name='.$i.' value='.$aantal[$i].' class="calculator-input" onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>
+    ';
+    print($aantal[$i]);
+    if(isset($_GET[$i])) {
+        $hoeveelheid = $_GET[$i];
        // array_push($aantal, $hoeveelheid);
         $aantal[$i] = $hoeveelheid;
     }
@@ -30,19 +37,18 @@ foreach($producten as $index => $waarde){
 }
 print_r($aantal);
 
+?>
+<button type="submit" value="submit">kies</button>
+
+</form>
 
 
-
-
+<?php
 $_SESSION["wwaantal"] = $aantal;
 $_SESSION["wwproducten"] = $producten;
 //wanneer butten gedrukt{
 //   $aantal[0] = button;
 //}
-
-
-
-
 
 
 ?>
