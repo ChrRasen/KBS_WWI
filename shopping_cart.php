1a63373eff628d16def32a695f621e1dd9fc7f40
JochemBaan <39567302+JochemBaan@users.noreply.github.com>
2019-11-27 16:05:16 +0100

    Update shopping_cart.php

--- shopping_cart.php
+++ shopping_cart.php
@@ -5,26 +5,26 @@
 session_start();
 $aantal = array();
 $producten = array("Jelle", "Christian", "Andy");
-if(isset ($_POST["erbij"])) {
-    $producten = $_POST["erbij"];
+if(isset ($_GET["erbij"])) {
+    $producten = $_GET["erbij"];
 }
 $i = 0;
-
+$hoeveelheid = array();
 
 
 foreach($producten as $index => $waarde){
 
-    echo '<form action="shopping_cart.php" method="post">
+    echo '<form action="shopping_cart.php" method="get">
     ' .$waarde.'
-<input type="number" min="1" name="hoeveelheid" value="1" id="'.$i.'" class="calculator-input" onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>
+<input type="number" min="1" name="'.$i.'" value="1" class="calculator-input" onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>
     <button type="submit" value="submit">kies</button>
     
     </form>';
-    if(isset($_POST["hoeveelheid"])) {
-        $hoeveelheid = $_POST["hoeveelheid"];
-       // array_push($aantal, $hoeveelheid);
-        $aantal[$i] = $hoeveelheid;
-    }
+    if(isset($_GET[$i])) {
+        $hoeveelheid[$i] = $_GET[$i];
+        array_push($aantal, $hoeveelheid);
+        //$aantal = array_replace($aantal ,$hoeveelheid);
+  //  }
     $i++;
 
 }
