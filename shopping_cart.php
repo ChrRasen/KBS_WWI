2e3fef2b152e0144b6502fed7db296a842035fad
JochemBaan <39567302+JochemBaan@users.noreply.github.com>
2019-11-27 11:56:19 +0100

    Update shopping_cart.php

--- shopping_cart.php
+++ shopping_cart.php
@@ -8,21 +8,28 @@ $producten = array("Jelle", "Christian", "Andy");
 if(isset ($_POST["erbij"])) {
     $producten = $_POST["erbij"];
 }
-
-
 $i = 0;
+
+
+
 foreach($producten as $index => $waarde){
 
     echo '<form action="shopping_cart.php" method="post">
     ' .$waarde.'
-<input type="number" min="1" name="hoeveelheid" value="1" class="calculator-input" onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>
+<input type="number" min="1" name="hoeveelheid" value="1" id="'.$i.'" class="calculator-input" onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>
     <button type="submit" value="submit">kies</button>
     
-    
-      
     </form>';
+    if(isset($_POST["hoeveelheid"])) {
+        $hoeveelheid = $_POST["hoeveelheid"];
+       // array_push($aantal, $hoeveelheid);
+        $aantal[$i] = $hoeveelheid;
+    }
     $i++;
+
 }
+print_r($aantal);
+
 
 
 
