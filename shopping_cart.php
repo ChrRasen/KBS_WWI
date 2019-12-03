41a8bc220b444e57dcda8eed68f12799002ed559
JochemBaan <39567302+JochemBaan@users.noreply.github.com>
2019-12-03 13:45:00 +0100

    Update shopping_cart.php

--- shopping_cart.php
+++ shopping_cart.php
@@ -2,49 +2,83 @@
 
 <body>
 <?php
+
 session_start();
-$aantal = array();
-$producten = array("Jelle", "Christian", "Andy");
-if(isset ($_GET["erbij"])) {
-    $producten = $_GET["erbij"];
+include "DatabaseConnection.php";
+$producten = array();
+$geldopslag = array();
+if (!($_SESSION["Ses_producten"] === null)){
+    $producten = $_SESSION["Ses_producten"];
 }
-$i = 0;
-$hoeveelheid = array();
+
+if(isset ($_GET["erbij"])  ) {
+    $erbij = $_GET["erbij"];
+    $producten["$erbij"] = 1;
+}
+
+foreach ($producten as $index => $waarde) {
+    if(isset ($_GET[$index])) {
+        $quant = $_GET[$index];
+        $producten[$index] = $quant;
+    }
+}
 
 
-foreach($producten as $index => $waarde){
+echo '<form action="shopping_cart.php" method="get">'; // Create form
+foreach($producten as $index => $waarde) {
+    $sqlquery = mysqli_query($connection, "SELECT StockItemName FROM stockitems WHERE StockItemID = $index");
+    if (!$sqlquery) {
+        printf("Error: %s\n", mysqli_error($connection));
+        exit();
+    }
+    if($waarde == null){
+        unset($producten[$index]);
+    }
+    $resultStockItemName = mysqli_fetch_array($sqlquery, MYSQLI_BOTH);
+    $naam = $resultStockItemName["StockItemName"];
 
-    echo '<form action="shopping_cart.php" method="get">
-    ' .$waarde.'
-<input type="number" min="1" name="'.$i.'" value="1" class="calculator-input" onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>
-    <button type="submit" value="submit">kies</button>
-    
+    $sqlquery2 = mysqli_query($connection, "SELECT UnitPrice FROM stockitems WHERE StockItemID = $index");
+
+    $resultUnitPrice = mysqli_fetch_array($sqlquery2, MYSQLI_BOTH);
+    $PrijsPerStuk = $resultUnitPrice["UnitPrice"];
+
+    echo  $naam .
+        '<input type="number" min="1" value="'.$waarde.'"  name="' . $index . '" class="calculator-input" 
+            onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>' ;
+    if(!($waarde == null)) {
+        $PrijsPerProduct = ($PrijsPerStuk * $waarde);
+
+        echo($waarde . " totaal: " . $PrijsPerProduct . "€");
+
+        echo '<br>';
+        $geldopslag[$index] = $PrijsPerProduct;
+    }else {
+        print(" weet u het zeker?");
+        echo '<br>';
+    }
+}
+
+
+$totaalbedrag = array_sum($geldopslag);
+if($totaalbedrag > 0) {
+    if ($totaalbedrag <= 40) {
+        $totaalbedrag = $totaalbedrag + 6.95;
+    }
+}
+print("uw totaal bedrag is: " . $totaalbedrag);
+
+
+echo '<button type="submit" value="submit">aanpassen</button> 
     </form>';
-    if(isset($_GET[$i])) {
-        $hoeveelheid[$i] = $_GET[$i];
-        array_push($aantal, $hoeveelheid);
-        //$aantal = array_replace($aantal ,$hoeveelheid);
-  //  }
-    $i++;
-
-}
-print_r($aantal);
-
-
-
-
-
-$_SESSION["wwaantal"] = $aantal;
-$_SESSION["wwproducten"] = $producten;
-//wanneer butten gedrukt{
-//   $aantal[0] = button;
-//}
-
-
-
-
-
-
+$_SESSION["Ses_producten"] = $producten;
+echo'
+   
+    <form action="betaalpagina.php" method="get"> 
+<button type="submit" name="afrekenen" value="'.$totaalbedrag.'"> afrekenen</button>
+        </form>';
 ?>
+
+<a href="Index.php">verder met winkelen</a>
+
 </body>
 </html>
