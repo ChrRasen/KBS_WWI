3d6a50c5cf3d7cc1306a580f9787f2334ecda498
ChrRasen <christianps23@hotmail.com>
2019-12-03 15:42:27 +0100

    Home pagina producten toegevoegd

--- Frontend/WWI Website/Home.php
+++ Frontend/WWI Website/Home.php
@@ -1,16 +1,13 @@
 <html>
 <head>
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=PT+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="all" href="style/Stylesheet.css">
+
+<?php include "index.php" ?>
 </head>
 <body>
 
 <div id="header"></div>
 <! -- Slideshow -->
-<div class="content">
+
 <div class="slideshow-container">
     <div class="mySlides fade">
         <div class="numbertext">1 / 3</div>
@@ -34,14 +31,21 @@
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 
-
+<div class="content">
 <div class="row">
-    <div class="row-1">Hallo Hallo Hallo Hallo Hallo Hallo Hallo Hallo</div>
-    <div class="row-2">Hallo Hallo Hallo Hallo Hallo Hallo Hallo Hallo</div>
-    <div class="row-3">Hallo Hallo Hallo Hallo Hallo Hallo Hallo Hallo</div>
+    <div class="row-1"><h2>Eerder bekeken</h2><?php
+        include "HomeProducten.php";
+        ?></div>
+    <div class="row-2"><h2>Aandachtstrekkers</h2><?php
+        include "HomeProducten.php";
+        ?></div>
+    <div class="row-3"><h2>Aanbiedingen</h2><?php
+        include "HomeProducten.php";
+        ?>
+    </div>
 </div>
 
-
+    <div class="clearFloat"></div>
 </div>
 
 <div id="footer"></div>
