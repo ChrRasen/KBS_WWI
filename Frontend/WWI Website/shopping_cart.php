784b3ea0842cd0d546909848ff1858aff8811f3e
JochemBaan <39567302+JochemBaan@users.noreply.github.com>
2019-12-04 11:02:10 +0100

    Update shopping_cart.php

--- Frontend/WWI Website/shopping_cart.php
+++ Frontend/WWI Website/shopping_cart.php
@@ -6,9 +6,9 @@
 session_start();
 include "DatabaseConnection.php";
 $producten = array();
-
+$geldopslag = array();
 if(empty($_SESSION["Ses_producten"])){
-    print('winkelwagen is nog leeg');
+    print('');
 }elseif (!($_SESSION["Ses_producten"] === null)){
     $producten = $_SESSION["Ses_producten"];
 }
@@ -24,7 +24,11 @@ foreach ($producten as $index => $waarde) {
         $producten[$index] = $quant;
     }
 }
-
+foreach($producten as $index => $waarde){
+if($waarde == null){
+    unset($producten[$index]);
+}
+}
 
 echo '<form action="shopping_cart.php" method="get">'; // Create form
 foreach($producten as $index => $waarde) {
@@ -33,7 +37,9 @@ foreach($producten as $index => $waarde) {
         printf("Error: %s\n", mysqli_error($connection));
         exit();
     }
-
+  //  if($waarde == null){
+   //     unset($producten[$index]);
+   // }
     $resultStockItemName = mysqli_fetch_array($sqlquery, MYSQLI_BOTH);
     $naam = $resultStockItemName["StockItemName"];
 
@@ -43,18 +49,41 @@ foreach($producten as $index => $waarde) {
     $PrijsPerStuk = $resultUnitPrice["UnitPrice"];
 
     echo  $naam .
-        '<input type="number" min="1" value="'.$waarde.'"  name="' . $index . '" class="calculator-input" 
+        '<input type="number" min="1" value="'.$waarde.'"  name="' . $index . '" class="calculator-input"
             onkeypress="return event.charCode >= 48 && event.charCode <= 57"></div>' ;
-    $PrijsPerProduct = ($PrijsPerStuk * $waarde);
-    echo(" " . $PrijsPerProduct);
-    echo'<br>';
+    if(!($waarde == null)) {
+        $PrijsPerProduct = ($PrijsPerStuk * $waarde);
 
+        echo($waarde . " totaal: " . $PrijsPerProduct . "€");
+
+
+        $geldopslag[$index] = $PrijsPerProduct;
+    }
+    echo '<br>';
 }
-echo '<button type="submit" value="submit">Toevoegen</button> 
+
+$totaalbedrag = array_sum($geldopslag);
+if($totaalbedrag > 0) {
+    if ($totaalbedrag <= 40) {
+        $totaalbedrag = $totaalbedrag + 6.95;
+    }
+}
+if($totaalbedrag == 0) {
+    print('uw winkelwagen is leeg' . '<br>');
+}
+print("uw totaal bedrag is: " . $totaalbedrag);
+
+echo '<button type="submit" value="submit">aanpassen</button>
     </form>';
 $_SESSION["Ses_producten"] = $producten;
+echo '
+
+    <form action="betaalpagina.php" method="get">
+<button type="submit" name="afrekenen" value="' .$totaalbedrag.'"> afrekenen</button>
+        </form>';
 ?>
-<a href="Index.php">verder met winkelen</a>
+
+<a href="Home.php">verder met winkelen</a>
 
 </body>
 </html>
