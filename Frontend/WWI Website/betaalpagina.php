cf6ebe319b1a1a91e2d53495277e219b455ceb9a
ChrRasen <christianps23@hotmail.com>
2019-12-16 17:59:05 +0100

    Images + Betaalpagina en stylesheet

--- Frontend/WWI Website/betaalpagina.php
+++ Frontend/WWI Website/betaalpagina.php
@@ -2,22 +2,24 @@
 
 <body>
 <div id="header"></div>
-        <div id="content">
+        <div class="content">
 
 
 <?php
 include "Index.php";
 ?>
-            <div style="" class="paymentMethod">
+            <div class="paymentMethod">
                 <div class="paymentHeader">
                     <ul>
-                    <li>Winkelwagen</li>
-                    <li>Betaalmethode</li>
+                        <li style="color: black "><a href="http://localhost/KBS_WWI/Frontend/WWI%20Website/shopping_cart.php">Winkelwagen</a></li>
+                    <li style="color: white; background-color: #62b4cf">Betaalmethode</li>
+                        <li style="color: black;">Order review</li>
                     </ul>
                 </div>
-                <input type="image" name="redirect" value="http://localhost/KBS_WWI/Frontend/WWI%20Website/Afgerondebetaling.php" src="images/Logo's/ideal.png" ">Ideal<br>
-                <input type="image" name="redirect" value="http://localhost/KBS_WWI/Frontend/WWI%20Website/Afgerondebetaling.php" src="images/Logo's/paypal.png" ">Paypal<br>
-                <input type="image" name="redirect" value="http://localhost/KBS_WWI/Frontend/WWI%20Website/Afgerondebetaling.php" src="images/Logo's/maestro.png" t">Maestro<br>
+                <input type="image" name="redirect" value="http://localhost/KBS_WWI/Frontend/WWI%20Website/Afgerondebetaling.php" src="images/Logo's/ideal.png" alt="submit" ">
+                <input type="image" name="redirect" value="http://localhost/KBS_WWI/Frontend/WWI%20Website/Afgerondebetaling.php" src="images/Logo's/paypal.png"alt="submit"  ">
+                <input type="image" name="redirect" value="http://localhost/KBS_WWI/Frontend/WWI%20Website/Afgerondebetaling.php" src="images/Logo's/maestro.png"alt="submit"style="
+    margin-bottom: 30  ">
 
             <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
             <script>
